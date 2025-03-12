2c72595e769ebe563ac3730bc878da4e542031ea
膜法使坚果墙 <96737835+jadgpanzer@users.noreply.github.com>
2025-03-12 17:19:38 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,15 @@ int main(int argc,char* argv[]) {
     Dataset sample_data;
     string path(argv[1]);
     sample_data.load_dataset(path);
-    sample_data.num_classes = 4;
-    RandomForest rf(100, 5, 4); // 100棵树，最大深度5，4个特征
+    sample_data.num_classes = 3;
+    RandomForest rf(100, 5, 3); // 100棵树，最大深度5，3个特征
     
     // 训练模型
     rf.train(sample_data);
     // 保存模型
     rf.save_model("forest.model");
     // 加载模型
-    RandomForest rf2(100, 5, 4);
+    RandomForest rf2(100, 5, 3);
     rf2.load_model("forest.model");
     // 预测示例（使用IRIS数据集，这里预测结果应该为2）
     Sample test_sample{{5.8, 2.8, 5.1, 2.4}};
